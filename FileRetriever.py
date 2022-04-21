ada60017a34eafe87746d60584fb5d59c8b0085b
Cyrill Gurtner <me@cgurtner.ch>
2022-04-21 13:44:18 +0200

    urlretrieve file

--- FileRetriever.py
+++ FileRetriever.py
@@ -1,7 +1,18 @@
-import os
+from urllib.request import urlretrieve
+from datetime import datetime
 
 class FileRetriever:
     DATA_RELATIVE_DIR_PATH = 'data/'
     
-    def __init__(self, url) -> None:
+    def __init__(self, url, name) -> None:
         self.__url = url
+        self.__name = name
+        self.update()
+    
+    def update(self) -> None:
+        filename = datetime.now().strftime('%m%d%Y_%H%M%S') + '_' + self.__name
+        urlretrieve(self.__url, FileRetriever.DATA_RELATIVE_DIR_PATH + filename)
+
+if __name__ == '__main__':
+    url = 'https://dam-api.bfs.admin.ch/hub/api/dam/assets/20964153/master'
+    fr = FileRetriever(url, 'lel.csv')
