77db66f2b3b014f43dc31388558970f48277ffd1
seitzm97 <seitz.michael97@hotmail.com>
2022-04-24 12:24:04 +0200

    added a plot calculating the avg unemployemt

--- main_app.py
+++ main_app.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Apr 23 10:08:06 2022
-
-@author: Seitz
+__author__ = "Michael Seitz"
+__email__ = "seitzmi1@students.zhaw.ch"
 """
+
 from FileRetriever import FileRetriever
 import pandas as pd
 import matplotlib.pyplot as plt
 
-url = 'https://dam-api.bfs.admin.ch/hub/api/dam/assets/20964153/master'
-file = FileRetriever()
-CANTON_CODES = [
+class main_application():
+    CANTON_CODES = [
 ['CH011','Waadt'],
 ['CH012','Wallis'],
 ['CH013','Genf'],
@@ -37,15 +36,9 @@ CANTON_CODES = [
 ['CH065','Nidwalden'],
 ['CH066','Zug'],
 ['CH070','Tessin'],]
-
-
-class main_application():
     def __init__(self):
-        self.canton_df = pd.DataFrame(CANTON_CODES, columns= ['code','canton'])
+        self.canton_df = pd.DataFrame(main_application.CANTON_CODES, columns= ['code','canton'])
         self.read_data()
-        self.ch_employed_by_year()
-        self.ch_unemployment_by_year()
-        self.avg_unemployment_per_canton_per_year()
         
     def read_data(self):
         self.data = pd.read_csv('data/erwerbsquote_nach_kanton.csv', sep = ';')
@@ -69,14 +62,34 @@ class main_application():
         temp_df = temp_df[~temp_df.ERWL.str.contains("0")]
         temp_df = temp_df[~temp_df.ERWL.str.contains('Total')]
         temp_df = temp_df[~temp_df.GEO.str.contains("0")]
-        self.temp_df = temp_df[temp_df['UNIT_MEA'].str.contains('%', na=False)]
+        temp_df = temp_df[temp_df['UNIT_MEA'].str.contains('%', na=False)]
         fig, ax = plt.subplots()
         ax.set(title='Arbeitslose Schweiz',ylabel= 'Anahl Arbeitslose in % der Erwerbspersonen', xlabel= 'Jahr')
-        ax.plot(list_of_years, self.temp_df['OBS_VALUE'])
+        ax.plot(list_of_years, temp_df['OBS_VALUE'])
     
-    #def unemployment_per_canton(self):
+    def avg_unemployment_per_canton(self):
+        temp_df = self.data
+        temp_df = temp_df[~temp_df.ERWP.str.contains("1")]
+        temp_df = temp_df[~temp_df.ERWL.str.contains('Total')]
+        temp_df = temp_df[~temp_df.ERWL.str.contains("0")]
+        temp_df = temp_df.dropna()
+        temp_df = temp_df[temp_df['UNIT_MEA'].str.contains('%', na=False)]
+        temp_df = temp_df[temp_df['GEO'].str.contains('0', na=False)]
+        temp_df = temp_df.groupby(['GEO'], sort=False)['OBS_VALUE'].mean()
+        temp_df = temp_df.to_frame().reset_index()
+        temp_df = temp_df.rename(columns = {'GEO':'code'})
+        temp_df = pd.merge(temp_df, self.canton_df, on = 'code', how= 'outer')
+        fig, ax = plt.subplots(figsize=(20,15))
+        ax.set(title = 'Druchschnittsarbeitslosigkeit von 2010-2020 pro Kanton', ylabel= 'Arbeitslose in %', xlabel= 'Kanton')
+        ax.bar(temp_df['code'], temp_df['OBS_VALUE'])
+        print('Canton-code legend:')
+        print(temp_df[['code', 'canton']])
         
-        
-a = main_application()
-aa = pd.read_csv('data/erwerbsquote_nach_kanton.csv', sep = ';')
+
+if __name__ == '__main__':
+    FileRetriever()
+    a = main_application()
+    a.ch_employed_by_year()
+    a.ch_unemployment_by_year()
+    a.avg_unemployment_per_canton()
 
